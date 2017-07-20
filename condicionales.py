65b111ca0e761a3fed42780a50ff3e1a69026918
dequiroga <de.quiroga@uniandes.edu.co>
2017-07-20 15:39:02 -0700

    Se perueba como meter coss al git con esta carpeta

---
+++ condicionales.py
@@ -0,0 +1,15 @@
+# condicionales 
+
+#If, if(condicion...): indentadolo que contiene el int. Todo lo que esta indentado es lo que ocurre si se cumple la condicon del if.
+
+if():
+
+#else if
+
+elif():
+
+# else
+
+else: 
+
+
---
+++ iteración.py
@@ -0,0 +1,15 @@
+# ejercicio de iteracion
+
+n=101
+i=1
+Numeros_Pares=[]
+
+while i<n:
+	if(i%2==0):
+		Numeros_Pares.append(i)
+	i=i+1
+
+print Numeros_Pares
+print len(Numeros_Pares)
+
+
